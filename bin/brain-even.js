6944355e7d24e0489d69f3e60f6c51369ddfcf8a
crazy <crazy@Ubuntu.myguest.virtualbox.org>
2023-09-13 00:28:44 +0300

    added brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+let isActive = true;
+let userPointsCounter = 0;
+const userTargetPoints = 3;
+let userName = '';
+const maxRandomNumber = 101;
+const evenDevider = 2;
+const positiveAnswer = 'yes';
+const negativeAnswer = 'no';
+
+console.log('Welcome to the Brain Games!');
+userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+console.log(`Answer "${positiveAnswer}" if the number is even, otherwise answer "${negativeAnswer}"`);
+
+while(isActive) {
+    const randomNumber = Math.floor(Math.random() * maxRandomNumber);
+
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    const isEven = randomNumber % evenDevider === 0 && userAnswer === positiveAnswer;
+    const isOdd = randomNumber % evenDevider !== 0 && userAnswer === negativeAnswer;
+    const isCorrect = isEven || isOdd;
+    const isEvenWrong = randomNumber % evenDevider === 0 && userAnswer !== positiveAnswer;
+    const isOddWrong = randomNumber % evenDevider !== 0 && userAnswer !== negativeAnswer;
+    const isWrong = isEvenWrong || isOddWrong;
+    
+    if (isCorrect) {
+        userPointsCounter += 1;
+        console.log('Correct!');
+    } else if (isWrong) {
+        isActive = false;
+        console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${isEvenWrong ? positiveAnswer : negativeAnswer}"`);
+    }
+
+    if (userPointsCounter === userTargetPoints) {
+        isActive = false;
+        console.log(`Congratulations, ${userName}!`);
+    }
+}
