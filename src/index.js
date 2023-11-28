4c51e827bd7ed1844faf6832326d9ee06dd09cf0
zigbul <zigbul@gmail.com>
2023-11-28 20:17:32 +0300

    even and calc games refactoring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import readlineSync from 'readline-sync';
 
 //general options
-let isActive = true;
 let userPointsCounter = 0;
 const userMaxPoints = 3;
 let userName = '';
 let userAnswer = '';
+const maxRandomNumber = 101;
 
 //even game options
-const maxRandomNumber = 101;
 const evenDevider = 2;
 const positiveAnswer = 'yes';
 const negativeAnswer = 'no';
@@ -21,8 +20,8 @@ const randomizer = (gap = 1) => {
     return Math.floor(Math.random() * gap);
 }
 
-const askQuestion = (random) => {
-    console.log(`Question: ${random}`);
+const askQuestion = (question) => {
+    console.log(`Question: ${question}`);
     userAnswer = readlineSync.question('Your answer: ');
 }
 
@@ -34,52 +33,48 @@ const printWrong = (userAnswer, result) => {
     console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${result}"`);
 }
 
-const endGame = () => {
-    isActive = false;
+const compareResult = (userAnswer, result) => {
+    if (userAnswer === result) {
+        userPointsCounter += 1;
+        printCorrect();
+    }
+    else {
+        userPointsCounter = userMaxPoints;
+        printWrong(userAnswer, result);
+    }
+}
+
+const printGameOver = () => {
     console.log(`Congratulations, ${userName}!`);
 }
 
+//main functions
 export const greeting = () => {
     console.log('Welcome to the Brain Games!');
     userName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${userName}`);
 }
 
-//main functions
 export const evenGame = () => {
     console.log(`Answer "${positiveAnswer}" if the number is even. otherwise answer "${negativeAnswer}"`);
 
-    while(isActive) {
+    while(userPointsCounter < userMaxPoints) {
         const randomNumber = randomizer(maxRandomNumber);
         askQuestion(randomNumber);
 
-        const isEven = randomNumber % evenDevider === 0 && userAnswer === positiveAnswer;
-        const isOdd = randomNumber % evenDevider !== 0 && userAnswer === negativeAnswer;
-        const isCorrect = isEven || isOdd;
-        const isEvenWrong = randomNumber % evenDevider === 0 && userAnswer !== positiveAnswer;
-        const isOddWrong = randomNumber % evenDevider !== 0 && userAnswer !== negativeAnswer;
-        const isWrong = isEvenWrong || isOddWrong;
+        const isEven = randomNumber % evenDevider === 0 
+        const result = isEven ? positiveAnswer : negativeAnswer;
 
-        const result = isEvenWrong ? positiveAnswer : negativeAnswer;
-
-        if (isCorrect) {
-            userPointsCounter += 1;
-            printCorrect();
-        } else if (isWrong) {
-            isActive = false;
-            printWrong(userAnswer, result);
-        }
-
-        if (userPointsCounter === userMaxPoints) {
-            endGame();
-        }
-    } 
+        compareResult(userAnswer, result);
+    }
+    
+    printGameOver();
 }
 
 export const calcGame = () => {
     console.log("What is the result of the expression? ");
 
-    while(isActive) {
+    while(userPointsCounter < userMaxPoints) {
         const operation = operations[randomizer(3)];
         const firstNumber = randomizer(maxRandomNumber);
         const secondNumber = randomizer(maxRandomNumber);
@@ -102,19 +97,8 @@ export const calcGame = () => {
                 break;
         }
 
-        const isCorrect = result.toString() === userAnswer;
-
-        if (isCorrect) {
-            userPointsCounter += 1;
-            printCorrect();
-        }
-        else {
-            isActive = false;
-            printWrong(userAnswer, result);
-        }
-
-        if (userPointsCounter === userMaxPoints) {
-            endGame();
-        }
+        compareResult(userAnswer, result.toString());
     }
+
+    printGameOver();
 }
