4a08fb02f0b2d49f211075a5244175a0c297eab4
zigbul <zigbul@gmail.com>
2024-01-06 21:04:30 +0300

    add all games in brain-games file

---
+++ bin/brain-games.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import even from '../src/games/even.js';
+import calc from "../src/games/calc.js";
+import gcd from '../src/games/gcd.js';
+import progression from '../src/games/progression.js';
+import prime from '../src/games/prime.js';
+
+even();
+calc();
+gcd();
+progression();
+prime();
