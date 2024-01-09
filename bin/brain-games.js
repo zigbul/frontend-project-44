4b079d9197cfb5fe51196b7a53f4f37ebe09bec9
zigbul <zigbul@gmail.com>
2024-01-09 19:36:37 +0300

    add makeWelcome function

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
+import makeWelcome from '../src/cli.js';
+
 import even from '../src/games/even.js';
 import calc from "../src/games/calc.js";
 import gcd from '../src/games/gcd.js';
 import progression from '../src/games/progression.js';
 import prime from '../src/games/prime.js';
+
+makeWelcome();
 
 even();
 calc();
---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const makeWelcome = () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+};
+
+export default makeWelcome;
